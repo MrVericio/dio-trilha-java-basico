95ec9c4dc9e4e953b0065c118a466e07ddb0d69f
MrVericio <marcosvericio@gmail.com>
2026-03-09 11:35:45 -0300

    commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class ContaTerminal {
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) throws Exception {
+
         // TODO: Conhecer e importar a classe Scanner
 
         // Exibir as mensagens para o usuário
@@ -22,7 +23,7 @@ public class ContaTerminal {
         String numeroAgencia = scanner.nextLine();
         System.out.println("Número da agência: " + numeroAgencia);
         // Receber o número da agência digitada pelo usuário e exibir no terminal
-        
+
         System.out.println("Digite o nome do cliente:");
         String nomeCliente = scanner.nextLine();
         System.out.println("Nome do cliente: " + nomeCliente);
@@ -30,12 +31,15 @@ public class ContaTerminal {
 
         double saldo = 523.02;
         System.out.println("Saldo da conta: " + saldo);
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
-        // Exibir a mensagem final para o cliente, seguindo o modelo: "Olá [nome do cliente], obrigado por criar uma conta em nosso banco, sua agência é [número da agência], conta [número da conta] e seu saldo [saldo da conta] já está disponível para saque."
+        System.out.println(
+                "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia
+                        + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        // Exibir a mensagem final para o cliente, seguindo o modelo: "Olá [nome do
+        // cliente], obrigado por criar uma conta em nosso banco, sua agência é [número
+        // da agência], conta [número da conta] e seu saldo [saldo da conta] já está
+        // disponível para saque."
         scanner.close();
         // Fechar o Scanner
     }
 
-        
-        
 }
